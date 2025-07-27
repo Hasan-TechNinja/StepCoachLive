f5ae9df0c7e0b00ccdf4e76064fddfb2c28f03e0
Hasan-TechNinja <hasantechninja@gmail.com>
2025-07-27 11:17:17 +0600

    timer bug fixed

--- main/signals.py
+++ main/signals.py
@@ -1,13 +1,15 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
-from .models import Profile
+from .models import Profile, Timer
+from django.utils import timezone
 
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     if created:
     
         Profile.objects.create(user=instance)
+        Timer.objects.create(user=instance, start_time=timezone.now())
         # OnboardingData.objects.create(user=instance)
     else:
         
